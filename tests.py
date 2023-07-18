23bc6a9f7304468f85a9cba69eba205e5c435779
Salvio53 <139824273+Salvio53@users.noreply.github.com>
2023-07-17 21:05:35 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,53 +6,35 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_index(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'<h1>Bienvenido</h1>', response.data)
-
     def test_correct_login_with_otp(self):
         response = self.app.post('/login', data=dict(
-            username='user1',
-            password='password123',
-            otp_code='123456'
+            username="user1",
+            password="password123",
+            otp_code="123456"
         ))
-self.assertIn(b"Inicio de sesión exitoso", response.data)
+        self.assertIn("Inicio de sesión exitoso", response.data.decode('utf-8'))
 
     def test_correct_login_without_otp(self):
         response = self.app.post('/login', data=dict(
-            username='user2',
-            password='qwerty456',
-            otp_code=''
+            username="user2",
+            password="qwerty456"
         ))
-        self.assertIn(b"Inicio de sesión exitoso", response.data)
+        self.assertIn("Inicio de sesión exitoso", response.data.decode('utf-8'))
 
-    def test_incorrect_username(self):
+    def test_incorrect_login_with_otp(self):
         response = self.app.post('/login', data=dict(
-            username='invalid_user',
-            password='password123',
-            otp_code='123456'
+            username="user1",
+            password="wrong_password",
+            otp_code="123456"
         ))
-        self.assertIn(b"Credenciales inválidas", response.data)
+        self.assertIn("Código OTP incorrecto", response.data.decode('utf-8'))
 
-    def test_incorrect_password(self):
+    def test_incorrect_login_without_otp(self):
         response = self.app.post('/login', data=dict(
-            username='user1',
-            password='wrong_password',
-            otp_code='123456'
+            username="user2",
+            password="wrong_password"
         ))
-        self.assertIn(b"Credenciales inválidas", response.data)
-
-    def test_incorrect_otp_code(self):
-        response = self.app.post('/login', data=dict(
-            username='user1',
-            password='password123',
-            otp_code='654321'  # OTP incorrecto
-        ))
-        self.assertIn(b"Código OTP incorrecto", response.data)
-
+        self.assertIn("Credenciales inválidas", response.data.decode('utf-8'))
 
 if __name__ == '__main__':
     unittest.main()
-
-
